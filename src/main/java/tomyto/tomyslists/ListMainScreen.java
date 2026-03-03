37c4b1aed4ced99bb8af1320cac8fd0eef132965
Tomyto536 <thomas.langdeau@gmail.com>
2026-03-03 13:39:26 -0500

    Ajout keybind pour ouvrir et fermer main screen

--- src/main/java/tomyto/tomyslists/ListMainScreen.java
+++ src/main/java/tomyto/tomyslists/ListMainScreen.java
@@ -1,14 +1,19 @@
 package tomyto.tomyslists;
 
 import java.awt.*;
+
 import java.util.concurrent.Flow;
 
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.input.KeyEvent;
 import io.wispforest.owo.ui.base.BaseUIModelScreen;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
 import io.wispforest.owo.ui.base.BaseOwoScreen;
 import io.wispforest.owo.ui.core.OwoUIAdapter;
 import io.wispforest.owo.ui.core.Surface;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import org.jetbrains.annotations.NotNull;
 
 import tomyto.tomyslists.tomyslistsClient;
@@ -18,15 +23,16 @@ import static tomyto.tomyslists.tomyslistsClient.openListMainScreenKey;
 public class ListMainScreen extends BaseOwoScreen<FlowLayout> {
 
     @Override
-    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        while (openListMainScreenKey.consumeClick()) {
-            if (client.screen == null) {
-                client.setScreen(new ListMainScreen());
-            }
+    public boolean keyPressed(KeyEvent input) {
+
+        if (openListMainScreenKey.matches(input)) {
+            Minecraft.getInstance().setScreen(null);
+            return true;
         }
+        return super.keyPressed(input);
+    }
 
-
-    @Override
+        @Override
     protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
         return OwoUIAdapter.create(this, Containers::verticalFlow);
     }
