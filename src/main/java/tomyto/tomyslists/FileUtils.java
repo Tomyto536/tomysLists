5385fad4915f3588920b7be7209c1c2593e8a9ec
Tomyto536 <thomas.langdeau@gmail.com>
2026-03-25 19:25:04 -0400

    Added candles to grouping

--- src/main/java/tomyto/tomyslists/FileUtils.java
+++ src/main/java/tomyto/tomyslists/FileUtils.java
@@ -223,7 +223,8 @@ public class FileUtils {
                     "copper|",
 
                     //Misc
-                    "lantern"
+                    "lantern",
+                    "candle"
 
             );
 
